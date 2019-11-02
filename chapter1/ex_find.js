a635c8a46b80f100b865b6eb3f1589f534222927
ssunny <ssssunny85@gmail.com>
2019-11-02 20:15:05 +0900

    Add READEME.md

--- chapter1/ex_find.js
+++ chapter1/ex_find.js
@@ -21,7 +21,7 @@ const users2 = [
 ];
 
 
-// find
+// find: 보조함수에 맞는 조건의 경우 바로 return 함으로써 함수가 종료된다. -> 결과값이 배열이 아닌 객체
 function find(list, callback) {
   for(let i = 0; i < list.length; i++) {
     if(callback(list[i])) return list[i];
@@ -30,3 +30,12 @@ function find(list, callback) {
 
 console.log(find(users2, function(user) { return user.getAge() === 25; }).getName()); // HA
 console.log(find(users2, function(user) { return user.getAge() < 30; }).getName()); // HA
+
+
+// findIndex: -1값은 매칭되는 값이 없는 경우, i는 보조함수에 맞는 조건의 경우 index를 리턴한다.
+function findIndex(list, predicate) {
+  for(let i = 0; i < list.length; i++) {
+    if(predicate(list[i])) return i;
+  }
+  return -1;
+}
